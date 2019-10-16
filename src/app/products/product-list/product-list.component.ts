5c881cec1ab807f70c70f2327513df4458c22838
Mohammed Dewidar <dewiidar@yahoo.com>
2019-10-16 03:57:43 +0200

    build action creators to strongly type actions & use action creators to create and dispatch actions

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,6 +6,8 @@ import {Product} from '../product';
 import {ProductService} from '../product.service';
 import {select, Store} from "@ngrx/store";
 import * as fromProduct from "../state/product.reducer"
+import * as ProductActions from "../state/product.actions"
+
 
 @Component({
     selector: 'pm-product-list',
@@ -55,7 +57,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
     }
 
     checkChanged(value: boolean): void {
-        this.store.dispatch({type: 'TOGGLE_PRODUCT_CODE', payload: value})
+        this.store.dispatch(new ProductActions.ToggleProductCode(value))
     }
 
     newProduct(): void {
